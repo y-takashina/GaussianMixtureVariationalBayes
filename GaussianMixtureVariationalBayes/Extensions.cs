9978deab5aca1d55d13efd9012a63a1c77ef3cc8
ytakashina <takashina2051@gmail.com>
2017-06-25 22:43:45 +0900

    add files

---
+++ GaussianMixtureVariationalBayes/Extensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GaussianMixtureVariationalBayes
+{
+    public static class Extensions
+    {
+        public static IEnumerable<(double, double)> ReadData(
+            string path,
+            (int i1, int i2) indices,
+            char delimiter = ',',
+            bool hasHeader = false
+        )
+        {
+            using (var sr = new StreamReader(path))
+            {
+                if (hasHeader)
+                {
+                    sr.ReadLine();
+                }
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(delimiter).Select(double.Parse).ToArray();
+                    yield return (line[indices.i1], line[indices.i2]);
+                }
+            }
+        }
+
+        public static void PlotPoints(
+            this Chart chart,
+            IEnumerable<(double x, double y)> points,
+            string name,
+            SeriesChartType chartType = SeriesChartType.Point,
+            ChartDashStyle dashStyle = ChartDashStyle.Solid,
+            MarkerStyle markerStyle = MarkerStyle.Circle,
+            int borderWidth = 2,
+            int markerSize = 3,
+            double xMin = double.NegativeInfinity,
+            double xMax = double.PositiveInfinity,
+            double yMin = double.NegativeInfinity,
+            double yMax = double.PositiveInfinity,
+            bool removeExisting = true
+        )
+        {
+            if (removeExisting)
+            {
+                chart.Series.Remove(chart.Series.FindByName(name));
+            }
+            if (chart.Series.All(series => series.Name != name))
+            {
+                chart.Series.Add(name);
+            }
+            chart.Series[name].ChartType = chartType;
+            chart.Series[name].BorderDashStyle = dashStyle;
+            chart.Series[name].MarkerStyle = markerStyle;
+            chart.Series[name].BorderWidth = borderWidth;
+            chart.Series[name].MarkerSize = markerSize;
+            if (!double.IsNegativeInfinity(xMin)) chart.ChartAreas[0].AxisX.Minimum = xMin;
+            if (!double.IsPositiveInfinity(xMax)) chart.ChartAreas[0].AxisX.Maximum = xMax;
+            if (!double.IsNegativeInfinity(yMin)) chart.ChartAreas[0].AxisY.Minimum = yMin;
+            if (!double.IsPositiveInfinity(yMax)) chart.ChartAreas[0].AxisY.Maximum = yMax;
+            foreach (var point in points)
+            {
+                chart.Series[name].Points.AddXY(point.x, point.y);
+            }
+        }
+    }
+}
--- GaussianMixtureVariationalBayes/Form1.cs
+++ GaussianMixtureVariationalBayes/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static GaussianMixtureVariationalBayes.Extensions;
 
 namespace GaussianMixtureVariationalBayes
 {
@@ -15,6 +18,9 @@ namespace GaussianMixtureVariationalBayes
         public Form1()
         {
             InitializeComponent();
+            chart1.Series.Clear();
+            var data = ReadData("GaussianMixtureData20130501.tsv", (0, 1), '\t');
+            chart1.PlotPoints(data, "observed data");
         }
     }
 }
